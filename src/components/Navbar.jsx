8d71ad5a7134f1b1a5220959726aa308753069a3
pj46280 <priyanshjha.pj@gmail.com>
2025-07-05 15:17:20 +0530

    Navbad Stylling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,10 +52,10 @@ export default function Example() {
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 border-none"
+            className="inline-flex items-center justify-center rounded-lg bg-white border-none p-2 text-gray-700 hover:shadow-sm hover:bg-gray-50 hover:shadow focus:outline-none"
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
@@ -81,16 +81,17 @@ export default function Example() {
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-0 z-50 flex justify-end">
+          <div className="w-full max-w-sm overflow-y-auto bg-white p-6 sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
 
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="inline-flex items-center justify-center rounded-lg bg-white border-none p-2 text-gray-700 hover:shadow-sm hover:bg-gray-50 hover:shadow focus:outline-none"
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
           <div className="mt-6 flow-root">
@@ -130,6 +131,7 @@ export default function Example() {
                 </a>
               </div>
             </div>
+          </div>
           </div>
         </DialogPanel>
       </Dialog>
